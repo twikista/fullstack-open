3ba314a6c26b64f19d578b6e6b4d812b01b7ea92
viro <twikista@gmail.com>
2023-04-09 04:47:30 +0100

    completed exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,17 +3,37 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const average = (good + 0 - bad) / total;
+  const positive = (good / total) * 100;
+
+  console.log(good, neutral, bad);
+
+  const increaseGood = () => {
+    setGood((good) => good + 1);
+  };
+
+  const increaseNeutral = () => {
+    setNeutral((neutral) => neutral + 1);
+  };
+
+  const increaseBad = () => {
+    setBad((bad) => bad + 1);
+  };
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={increaseGood}>good</button>
+      <button onClick={increaseNeutral}>neutral</button>
+      <button onClick={increaseBad}>bad</button>
 
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive + "%"}</p>
     </div>
   );
 };
