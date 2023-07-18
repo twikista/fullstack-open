8c7bb8e02d83c26d80fcc70115cb43a9708b129c
Aaron Anama <twikista@gmail.com>
2023-07-18 05:07:28 +0100

    implemented async action createor to fetch data from local server

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anectdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
@@ -26,4 +27,11 @@ const anecdoteSlice = createSlice({
 
 export const { createAnecdote, voteAnecdote, setAnecdotes } =
   anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anectdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
 export default anecdoteSlice.reducer
